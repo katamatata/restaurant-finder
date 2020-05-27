b14c55ca3e8d679db9c9c38d92e4550f3d49c57e
katamatata <51786805+katamatata@users.noreply.github.com>
2020-05-27 12:18:40 +0200

    Add components folder and Header component, add styled-components dependencies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import Header from './components/Header';
+
 function App() {
   return (
     <div className="App">
-      hello
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Header = () => (
+  <div>
+    <h1>Logo</h1>
+  </div>
+);
